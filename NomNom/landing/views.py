c6db200762c72e3cd9f8db43b391e3c75bb82e91
Sunhaloo <azmaan151003@gmail.com>
2026-04-02 15:16:50 +0400

    U: Toast for order success now appears on landing page ( after redirection )

--- NomNom/landing/views.py
+++ NomNom/landing/views.py
@@ -1,10 +1,22 @@
 from django.shortcuts import render
 from pastry.models import Pastry
 
+
 def index(request):
     try:
-        fudgy_cake = Pastry.objects.get(pastry_name='Chocolate Cake') 
+        fudgy_cake = Pastry.objects.get(pastry_name="Chocolate Cake")
     except Pastry.DoesNotExist:
         fudgy_cake = None
 
-    return render(request, "landing/landing.html", {'fudgy_cake': fudgy_cake})
+    order_success = request.GET.get("order_success") == "1"
+    order_id = request.GET.get("order_id")
+
+    return render(
+        request,
+        "landing/landing.html",
+        {
+            "fudgy_cake": fudgy_cake,
+            "order_success": order_success,
+            "order_id": order_id,
+        },
+    )
