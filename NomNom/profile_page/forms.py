12854ce88c1e7dbfe053d5716a2fa850ac33b21e
Sunhaloo <azmaan151003@gmail.com>
2026-04-02 15:39:22 +0400

    U: Removed full name from edit profile + increased the height of gender dropdown menu

--- NomNom/profile_page/forms.py
+++ NomNom/profile_page/forms.py
@@ -4,9 +4,8 @@ from login.models import User
 class EditProfileForm(forms.ModelForm):
     class Meta:
         model = User
-        fields = ["full_name", "username", "first_name", "last_name", "email", "gender", "region", "street", "profile_pic"]
+        fields = ["username", "first_name", "last_name", "email", "gender", "region", "street", "profile_pic"]
         widgets = {
-            "full_name": forms.TextInput(attrs={"class": "form-control"}),
             "username": forms.TextInput(attrs={"class": "form-control"}),
             "first_name": forms.TextInput(attrs={"class": "form-control"}),
             "last_name": forms.TextInput(attrs={"class": "form-control"}),
@@ -16,3 +15,11 @@ class EditProfileForm(forms.ModelForm):
             "street": forms.TextInput(attrs={"class": "form-control"}),
             "profile_pic": forms.FileInput(attrs={"class": "form-control"}),
         }
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        # Keep full_name in sync with first_name + last_name without exposing the field in the form
+        user.full_name = f"{user.first_name} {user.last_name}".strip()
+        if commit:
+            user.save()
+        return user
