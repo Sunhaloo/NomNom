44d55f27de57b171fde616c425cbd032871a2a69
Sunhaloo <azmaan151003@gmail.com>
2026-04-06 22:49:54 +0400

    U: Super-admin now sees every single table in admin panel

--- NomNom/payments/admin.py
+++ NomNom/payments/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Payment
+
+
+@admin.register(Payment)
+class PaymentAdmin(admin.ModelAdmin):
+    list_display = [
+        "payment_id",
+        "order",
+        "payment_status",
+        "payment_method",
+        "amount",
+        "payment_date",
+    ]
+    list_filter = ["payment_status", "payment_method", "payment_date"]
+    search_fields = ["payment_id", "order__id"]
--- NomNom/review/admin.py
+++ NomNom/review/admin.py
@@ -2,4 +2,8 @@ from django.contrib import admin
 from .models import Review
 
 
-# Register your models here.
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ["id", "user", "pastry", "rating", "date"]
+    list_filter = ["rating", "date"]
+    search_fields = ["user__username", "pastry__pastry_name", "comment"]
