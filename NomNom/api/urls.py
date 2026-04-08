23614b4dbbecff2b9e8278ad27b97e6b892fd1db
Sunhaloo <azmaan151003@gmail.com>
2026-04-08 14:43:22 +0400

    U: Completed API creation of delivery

--- NomNom/api/urls.py
+++ NomNom/api/urls.py
@@ -1,10 +1,12 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from .views import OrderViewSet
+from .views import OrderViewSet, DeliveryViewSet
+
 
 router = DefaultRouter()
 router.register(r"orders", OrderViewSet, basename="order")
+router.register(r"deliveries", DeliveryViewSet, basename="delivery")
 
 
 urlpatterns = [
