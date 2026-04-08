daae3cad7f0424a28a047dba06d437be67ca1924
Sunhaloo <azmaan151003@gmail.com>
2026-04-08 14:43:22 +0400

    U: Completed API creation of delivery

--- NomNom/api/serializers.py
+++ NomNom/api/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 
 from orders.models import Order
+from delivery.models import Delivery
 
 
 class OrderSerializer(serializers.ModelSerializer):
@@ -15,4 +16,34 @@ class OrderSerializer(serializers.ModelSerializer):
             "order_status",
             "total_amount",
             "is_preorder",
+        ]
+
+
+class OrderSummaryForDeliverySerializer(serializers.ModelSerializer):
+
+    class Meta:
+        # get the data from the 'order' app
+        model = Order
+        # fields to be accessible through the API
+        fields = [
+            "id",
+            "order_date",
+            "order_status",
+            "total_amount",
+        ]
+
+
+class DeliverySerializer(serializers.ModelSerializer):
+    order = OrderSummaryForDeliverySerializer(read_only=True)
+
+    class Meta:
+        # get the data from the 'delivery' app
+        model = Delivery
+        # fields to be accessible through the API
+        fields = [
+            "id",
+            "address",
+            "date",
+            "status",
+            "order",
         ]
