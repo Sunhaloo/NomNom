0d8004c924b30a3002da37cb67e53360a3dcdd5c
MilindGit21 <milindboodhun21@gmail.com>
2026-04-20 09:01:25 +0400

    B: tried doing more stuff

--- NomNom/NomNom/urls.py
+++ NomNom/NomNom/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
-from rest_framework.authtoken.views import obtain_auth_token
+#from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     # WARNING: the order of "operations" matter
@@ -18,6 +18,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("pastry/", include("pastry.urls")),
     path("profile/", include("profile_page.urls")),
-    path("api/v1/auth/token/", obtain_auth_token, name="api-token-auth"),
     path("api/v1/", include("api.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
